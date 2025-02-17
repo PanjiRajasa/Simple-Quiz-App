5c1d9beedf59d055622d20bb5112cf90cccf5b78
Panji Rajasa <igustipanjirajasa@gmail.com>
2025-02-17 11:56:03 +0800

    backup data

--- app/src/main/java/com/example/simplequizapp/ui/QuestionActivity.kt
+++ app/src/main/java/com/example/simplequizapp/ui/QuestionActivity.kt
@@ -1,11 +1,13 @@
 package com.example.simplequizapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.simplequizapp.R
+import com.example.simplequizapp.utils.Constant
 
 class QuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +19,9 @@ class QuestionActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+//        variable declaration
+        val questionsList = Constant.getQuestion()
+        Log.d("Question size", "${questionsList.size}")
     }
 }
